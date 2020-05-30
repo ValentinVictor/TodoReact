dd1e29d0a813e39fef6c4365a0bba69999bf6513
Volker Pfaff <volker.pfaff@envinet.com>
2020-05-30 23:19:44 +0200

    Updates

---
+++ precache-manifest.dcc60978ec308c3f7b460879b7706ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c5d3a16f4cbac80d0801d3e83d0b9c",
+    "url": "/TodoReact/index.html"
+  },
+  {
+    "revision": "0009b3016aeb0f827c21",
+    "url": "/TodoReact/static/css/main.e6fe31cf.chunk.css"
+  },
+  {
+    "revision": "e65f3aeb205f777adc07",
+    "url": "/TodoReact/static/js/2.48865510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoReact/static/js/2.48865510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0009b3016aeb0f827c21",
+    "url": "/TodoReact/static/js/main.0422b90e.chunk.js"
+  },
+  {
+    "revision": "f43240e56c395ec667a7",
+    "url": "/TodoReact/static/js/runtime-main.59a88dfd.js"
+  }
+]);
