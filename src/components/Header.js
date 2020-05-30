a781b13c37da5d165e210586e5237b8fc858d552
Volker Pfaff <volker.pfaff@envinet.com>
2020-05-30 21:43:47 +0200

    'initial'

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+
+const Header = () => {
+    const headerStyle = {
+        padding: "20px 0",
+        lineHeight: "2em",
+      }  
+  return (
+    <header style={headerStyle}>
+      <h1 style={{ fontSize: "25px", marginBottom: "15px" }}> My First React App</h1>
+      <p style={{ fontSize: "14px" }}>Please add to-dos item(s) through the input field</p>
+    </header>
+  )
+}
+
+export default Header
